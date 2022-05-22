d97cdb0469e5e841cb7b3f66a4bc1bbb1e225267
KinozHao <kinozhao0715@gmail.com>
2022-05-22 19:53:51 +0800

    improve

--- d_Collections/src/c_map/MapMethod.java
+++ d_Collections/src/c_map/MapMethod.java
@@ -1,38 +1,47 @@
 package c_map;
 
+import jdk.swing.interop.SwingInterOpUtils;
 import org.junit.Test;
 import java.util.*;
 
 public class MapMethod {
     @Test
-    public void Collections() {     //获取
-        HashMap<String,Integer> map=new HashMap<>();
-        map.put("Po",13);        //键对值
-        map.put("Lo",14);
-        map.put("Ho",15);
-        Collection<Integer> integers = map.values();  //获取集合中所有值的集合
-        System.out.println(integers);
-    }
-    @Test
-    public void Judge() {   //判断
-        HashMap<String,Integer> po=new HashMap<>();
-        po.put("Po",13);        //键对值
-        po.put("Lo",14);
-        po.put("Ho",15);
-        //判断是否包含
-        System.out.println(po.containsKey("Po"));
-        System.out.println(po.containsValue(15));
-        System.out.println(po.isEmpty());   //判断是否为空
-    }
-    @Test
-    public void putAndclear() { //添加
-        HashMap<String,Integer> go=new HashMap<>();
-        go.put("Po",13);        //键对值
+    //增删改
+    public void Method1() {
+        HashMap go=new HashMap<>();
+        go.put("Po",13);
         go.put("Lo",14);
         go.put("Ho",15);
+        go.put("Ho",15333); //修改
+        go.remove("Po",13); //移除
         System.out.println(go);
-        go.clear();
+        System.out.println("key-value的个数:"+go.size());
+        //putAll方法的体现
+        HashMap go2 = new HashMap();
+        go2.putAll(go);
+        go2.put("HH",22);
+        System.out.println(go2);
+        go.clear();//清除
         System.out.println(go);
+    }
+    @Test
+    //判断
+    public void Judge(){
+        HashMap map = new HashMap();
+        map.put("A",0);
+        map.put("B",1);
+        map.put("C",2333);
+        //获取指定key对应的value
+        System.out.println(map.get("C"));
+
+        //判断Map中是否包含指定key和value
+        System.out.println(map.containsKey("A"));
+        System.out.println(map.containsValue("2333"));
+        System.out.println(map.isEmpty());  //判断是否为空
+
+        //判断两个map中数据是否全部相等
+        System.out.println(map.equals(new HashMap()));
+
     }
 
 }
--- d_Collections/src/c_map/MapTravers.java
+++ d_Collections/src/c_map/MapTravers.java
@@ -1,42 +1,70 @@
 package c_map;
+import org.junit.Test;
+import z_tools.CollectionNote;
+
+import javax.print.attribute.standard.NumberOfInterveningJobs;
 import java.util.*;
-//Map集合的遍历体现
+@CollectionNote("关于Map遍历的实例")
 public class MapTravers {
-    public static void main(String[] args) {
-        //迭代器
-        Map<String,Integer> go=new HashMap<>();
-        go.put("Po",13);        //键对值
-        go.put("Lo",14);
-        go.put("Ho",15);
-        Set<String> keySet=go.keySet();     //获取所有的key
-        Iterator<String> in=keySet.iterator();  //获取迭代器
-        while (in.hasNext()){                   //判断集合中是否有元素
-            String key=in.next();               //获取每一个key
-            Integer value=go.get(key);          //根据key获取value
-            System.out.println(key+"-->"+value);    //结果
-        }
-        //增强for
-        for (String key:go.keySet()){           //go.keySet是所有key的集合
-            System.out.println(key+"-->"+go.get(key));
+    @Test
+    public void TraversAllValues(){
+        HashMap map = new HashMap();
+        map.put("河南",41);
+        map.put("江苏",32);
+        map.put("山东",34);
+        map.put("浙江",33);
+        //1.values方法获取所有value数据
+        //2.通过Collection方法粗放
+        //3.并对collection1方法进行遍历
+        Collection mapV = map.values();
+        for (Object obj : mapV) {
+            System.out.println(obj);
         }
     }
-    //Map集合的遍历之键值对对象找键和值
-    private static void AnotherMethod(){
+    @Test
+    public void TraversAllKeys(){
+        HashMap map = new HashMap();
+        map.put("河南",41);
+        map.put("江苏",32);
+        map.put("山东",34);
+        map.put("浙江",33);
+        Set set = map.keySet();
+        for(Object obj:set){
+            System.out.println(obj);
+        }
+    }
+    //键值对同时遍历的两种方法
+    @Test
+    public void GetMethod(){
+        //1.创建Map集合
+        Map<String,Integer> datas = new HashMap<>();
+        datas.put("Po",13);
+        datas.put("Lo",14);
+        datas.put("Ho",15);
+        Set<String> keySet = datas.keySet();
+        Iterator<String> iterator = keySet.iterator();
+        while (iterator.hasNext()){
+            //key值
+            Object key = iterator.next();
+            //通过key获取value
+            Object value = datas.get(key);
+            System.out.println(key+"-->"+value);
+        }
+    }
+
+    @Test
+    public void EntrySet(){
         Map<String,Integer> in=new HashMap<>();
         in.put("张三",23);
         in.put("李四",24);
         in.put("王五",25);
-
-        //Map.Entry说明Entry是Map的内部接口,将键和值封装成了Entry对象,并存储在Set集合中
-        Set<Map.Entry<String,Integer>> en=in.entrySet();
-        //迭代器的泛型和Set集合的泛型保持一致
-        Iterator<Map.Entry<String,Integer>> it=en.iterator();
-        while (it.hasNext()){           //判断集合中是否有元素
-            Map.Entry<String,Integer> go=it.next(); //获取每一个key
-            //Entry中的getKey/getValue 分别用来获取 键和值
-            System.out.println(go.getKey()+"="+go.getValue());
+        Set<Map.Entry<String, Integer>> entries = in.entrySet();
+        Iterator<Map.Entry<String, Integer>> iterator = entries.iterator();
+        while (iterator.hasNext()){
+            Map.Entry<String, Integer> next = iterator.next();
+            Map.Entry mey = next;
+            System.out.println(mey.getKey()+"->"+mey.getValue());
         }
-
         //定义一个临时变量 类型为Entry对象   entrySet获得元素
         for (Map.Entry<String,Integer> uc :in.entrySet()){
             System.out.println(uc.getKey()+"="+uc.getValue());
